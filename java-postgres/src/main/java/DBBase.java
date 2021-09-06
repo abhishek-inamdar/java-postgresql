84c91f1a8a34324e697a2b7dd16f0e51f7bb49f4
abhishek-inamdar <inamdarabhishek.191195@gmail.com>
2021-09-06 16:32:39 -0400

    DB connection and initial setup

---
+++ java-postgres/src/main/java/DBBase.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This class establishes DB connection based on database properties
+ *
+ * @author Abhishek Inamdar
+ */
+public class DBBase {
+    /**
+     * Database Properties file name
+     */
+    private static final String DB_PROPERTY_FILE = "db.properties";
+    /**
+     * Connection Object
+     */
+    private static Connection connection = null;
+
+    /**
+     * Returns Connection
+     *
+     * @return Connection object
+     */
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * Establishes connection based on database properties
+     *
+     * @throws SQLException If an SQL Error occurs
+     */
+    public static void establishConnection() throws SQLException {
+        if (Objects.isNull(connection)) {
+            // Read the properties file
+            // it should contain following properties
+            // URL, USERNAME, PASSWORD
+            File dbProperties = null;
+            try {
+                DBBase base = new DBBase();
+                dbProperties = base.getFileFromResource(DB_PROPERTY_FILE);
+            } catch (URISyntaxException e) {
+                System.err.println("Error loading db.properties file" + e.getMessage());
+            }
+            String url = null, username = null, password = null;
+
+            if (!Objects.isNull(dbProperties)) {
+                try {
+                    List<String> lines = Files.readAllLines(dbProperties.toPath(),
+                            StandardCharsets.UTF_8);
+                    for (String line : lines) {
+                        String[] parts = line.split("==");
+                        String property = parts[0];
+                        String value = null;
+                        if (parts.length > 1) {
+                            value = parts[1];
+                        }
+                        switch (property) {
+                            case "URL":
+                                url = value;
+                                break;
+                            case "USERNAME":
+                                username = value;
+                                break;
+                            case "PASSWORD":
+                                password = value;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error reading db.properties file");
+                }
+            }
+
+            if (url != null && username != null) {
+                connection = DriverManager.getConnection(url, username, password);
+            }
+            if (Objects.isNull(connection)) {
+                System.err.println("Can not establish the DB connection");
+            }
+        }
+    }
+
+    /**
+     * Utility method to read file from the resources
+     *
+     * @param fileName name of the file
+     * @return File object
+     * @throws URISyntaxException In case of File URI Syntax error
+     */
+    private File getFileFromResource(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+    }
+}
+
---
+++ java-postgres/src/main/java/DBSetup.java
@@ -0,0 +1,83 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
+/**
+ * Utility class to setup schema by creating tables
+ *
+ * @author Abhishek Inamdar
+ */
+public class DBSetup extends DBBase {
+    static final String DROP_TABLES = "DROP TABLE IF EXISTS ORDER_DETAILS, ORDERS, REVIEWS, PRODUCTS, USERS";
+    static final String CREATE_USERS_TABLE =
+            "CREATE TABLE USERS (" +
+                    "  USER_NAME VARCHAR(15)," +
+                    "  PASSWORD VARCHAR(15) NOT NULL," +
+                    "  FIRST_NAME TEXT NOT NULL," +
+                    "  LAST_NAME TEXT NOT NULL," +
+                    "  PRIMARY KEY (USER_NAME));";
+    static final String CREATE_PRODUCTS_TABLE =
+            "CREATE TABLE PRODUCTS (" +
+                    "  PRODUCT_ID VARCHAR(15)," +
+                    "  NAME TEXT NOT NULL," +
+                    "  DESCRIPTION TEXT," +
+                    "  PRICE NUMERIC NOT NULL CHECK (PRICE > 0)," +
+                    "  STOCK INTEGER NOT NULL CHECK (STOCK > 0)," +
+                    "  PRIMARY KEY (PRODUCT_ID));";
+    static final String CREATE_REVIEWS_TABLE =
+            "CREATE TABLE REVIEWS (" +
+                    "  USER_NAME VARCHAR(15)," +
+                    "  PRODUCT_ID VARCHAR(15)," +
+                    "  REVIEW_TEXT TEXT," +
+                    "  RATING FLOAT NOT NULL CHECK (RATING > 0) CHECK (RATING < 5)," +
+                    "  REVIEW_DATE TIMESTAMP NOT NULL," +
+                    "  PRIMARY KEY (USER_NAME, PRODUCT_ID)," +
+                    "  FOREIGN KEY (USER_NAME) REFERENCES USERS(USER_NAME)," +
+                    "  FOREIGN KEY (PRODUCT_ID) REFERENCES PRODUCTS(PRODUCT_ID));";
+    static final String CREATE_ORDERS_TABLE =
+            "CREATE TABLE ORDERS (" +
+                    "  ORDER_ID VARCHAR(15)," +
+                    "  USER_NAME VARCHAR(15) NOT NULL," +
+                    "  ORDER_DATE TIMESTAMP NOT NULL," +
+                    "  PRIMARY KEY (ORDER_ID)," +
+                    "  FOREIGN KEY (USER_NAME) REFERENCES USERS(USER_NAME));";
+    static final String CREATE_ORDER_DETAILS_TABLE =
+            "CREATE TABLE ORDER_DETAILS (" +
+                    "  ORDER_ID VARCHAR(15)," +
+                    "  PRODUCT_ID VARCHAR(15)," +
+                    "  QUANTITY INTEGER NOT NULL CHECK (QUANTITY > 0)," +
+                    "  PRIMARY KEY (ORDER_ID, PRODUCT_ID)," +
+                    "  FOREIGN KEY (ORDER_ID) REFERENCES ORDERS(ORDER_ID)," +
+                    "  FOREIGN KEY (PRODUCT_ID) REFERENCES PRODUCTS(PRODUCT_ID));";
+
+    /**
+     * Recreates all the required tables
+     *
+     * @throws SQLException If an SQl Error occurs
+     */
+    public void createTables() throws SQLException {
+        if (Objects.isNull(getConnection())) {
+            establishConnection();
+        }
+        Connection con = getConnection();
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            stmt.execute(DROP_TABLES);
+            stmt.execute(CREATE_USERS_TABLE);
+            stmt.execute(CREATE_PRODUCTS_TABLE);
+            stmt.execute(CREATE_REVIEWS_TABLE);
+            stmt.execute(CREATE_ORDERS_TABLE);
+            stmt.execute(CREATE_ORDER_DETAILS_TABLE);
+        } finally {
+            try {
+                if (!Objects.isNull(stmt)) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Something went REALLY wrong.");
+            }
+        }
+    }
+}
